8576c8283be57e06b2778498cc32bcb31b752d93
kanobi <e.robin154@gmail.com>
2024-09-01 18:19:31 +0200

    web requests, handling exceptions

--- src/async_web/gather_sessions.py
+++ src/async_web/gather_sessions.py
@@ -3,13 +3,14 @@ from aiohttp import ClientSession, ClientTimeout
 
 from src.util import async_timed, fetch_status
 
+TOTAL_REQS = 3000
 
 @async_timed()
 async def main():
     session_timeout = ClientTimeout(total=60, connect=0.1)
     request_timeout = ClientTimeout(total=10)
     async with ClientSession(timeout=session_timeout) as session:
-        urls = ["https://www.example.com" for _ in range(3000)]
+        urls = ["https://www.example.com" for _ in range(TOTAL_REQS)]
         requests = [fetch_status(session, url, request_timeout) for url in urls]
         status_codes = await asyncio.gather(*requests)
         print(status_codes)
--- src/util/web.py
+++ src/util/web.py
@@ -1,8 +1,16 @@
+import asyncio
 from aiohttp import ClientSession, ClientTimeout
 from src.util import async_timed
 
 
 @async_timed()
-async def fetch_status(session: ClientSession, url: str, timeout: ClientTimeout) -> int:
+async def fetch_status(
+    session: ClientSession,
+    url: str,
+    timeout: ClientTimeout = None,
+    delay: int=0,
+) -> int:
+    if delay:
+        await asyncio.sleep(delay)
     async with session.get(url, timeout=timeout) as result:
         return result.status
